da228ae665d4c9943aff4fabb536ca9b54788c1d
dnzk <danishkalesaran@gmail.com>
2017-02-07 01:05:44 +0700

    set possible actions

--- src/action/index.js
+++ src/action/index.js
@@ -1,3 +1,59 @@
+// action types
+
+export const ADD_PRODUCT_LIST = 'ADD_PRODUCT_LIST';
+export const REMOVE_PRODUCT_LIST = 'REMOVE_PRODUCT_LIST';
+export const UPDATE_QTY_PRODUCT_LIST = 'UPDATE_QTY_PRODUCT_LIST';
+export const UPDATE_DISCOUNT_PRODUCT_LIST = 'UPDATE_DISCOUNT_PRODUCT_LIST';
+export const UPDATE_PRICE_PRODUCT_LIST = 'UPDATE_PRICE_PRODUCT_LIST';
+export const SET_MODAL_VISIBILITY = 'SET_MODAL_VISIBILITY';
+
+// action creators
+
+export function addProductToList(product) {
+  return {
+    type: ADD_PRODUCT_LIST,
+    product,
+  };
+}
+
+export function removeProductFromList(sku) {
+  return {
+    type: REMOVE_PRODUCT_LIST,
+    sku,
+  };
+}
+
+export function updateProductQty(sku, qty) {
+  return {
+    type: UPDATE_QTY_PRODUCT_LIST,
+    sku,
+    qty,
+  };
+}
+
+export function updateProductDiscount(sku, discount) {
+  return {
+    type: UPDATE_DISCOUNT_PRODUCT_LIST,
+    sku,
+    discount,
+  };
+}
+
+export function updateProductPrice(sku, price) {
+  return {
+    type: UPDATE_PRICE_PRODUCT_LIST,
+    sku,
+    price,
+  };
+}
+
+export function setModalVisibility(visible) {
+  return {
+    type: SET_MODAL_VISIBILITY,
+    visible,
+  };
+}
+
 // tree shape
 const INITIAL_STATE = {
   ui: {
