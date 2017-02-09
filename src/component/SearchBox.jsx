88661d966ea7b3588a2b5d31d404d04a67b2f9ac
Danish Kalesaran <dnzk@users.noreply.github.com>
2017-02-09 12:39:22 +0700

    Task refine template (#7)
    * style up search result
    
    * style data grid
    
    * attempt to fix font - failed

--- src/component/SearchBox.jsx
+++ src/component/SearchBox.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 
 const Product = (props) => (
-  <div className="l-flex c-box">
-    <span>
+  <div className="l-flex c-box c-box--no-gap">
+    <span className="l-flex__2 l-flex__align t--c-4">
       { props.product.sku }
     </span>
-    <span>
-      { props.product.name }
+    <span className="l-flex__2 l-flex__align t--c-1">
+      { props.product.description }
     </span>
-    <span>
+    <span className="l-flex__2 l-flex__align t--center t--c-1">
       { props.product.brand }
     </span>
-    <span>
+    <span className="l-flex__2 l-flex__align t--center t--c-1">
       { props.product.price }
     </span>
-    <button onClick={props.onPick.bind(this, props.product.sku)}>
-      Add
-    </button>
+    <div className="l-flex__1 l-flex--spread l-flex__align l-flex">
+      <input className="c-input--bordered c-input--small" type="text" />
+      <button className="c-button c-button--grey" onClick={props.onPick.bind(this, props.product.sku)}>
+        Add
+      </button>
+    </div>
   </div>
 )
 
@@ -34,9 +37,9 @@ class SearchBox extends React.Component {
     });
 
     return (
-      <div className="c-box">
+      <div className="c-box c-search-box">
         <input type="text" onChange={this.props.filterProducts.bind(this)} />
-        <div className="">
+        <div className="c-search-box__result-box">
           { products }
         </div>
       </div>
